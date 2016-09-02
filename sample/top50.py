3586527fedf5dd9e15199918614e87d03a1cab04
William Guo <yueguo@ebay.com>
2016-09-01 18:42:38 -0700

    sh sz dataframe

--- sample/top50.py
+++ sample/top50.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
 import re
+import pandas as pd
 
 from core import ashare
 
@@ -9,13 +10,14 @@ dict = ashare()
 sz50 = requests.get('http://qt.gtimg.cn/r=0.9452569935440971q=aszzdftop50')
 sz50s = re.findall(r"\d{6}",sz50.text)
 sz50names = map(lambda x: dict.get(x),sz50s)
-for s in sz50names:
-    print s
 
-print '------------'
 
 sh50 = requests.get('http://qt.gtimg.cn/r=0.9452569935440971q=ashzdftop50')
 sh50s = re.findall(r"\d{6}",sh50.text)
 sh50names = map(lambda x: dict.get(x),sh50s)
-for s in sh50names:
-    print s
+
+d = {'sz': sz50names, 'sh': sh50names}
+
+df = pd.DataFrame(data=d)
+
+print df
