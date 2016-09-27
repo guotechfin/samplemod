a4fe2f206f244b9cbd6a2cb55d8fb4ee29816e96
William Guo <yueguo@ebay.com>
2016-09-27 15:07:20 -0700

    tmp

--- sample/report.py
+++ sample/report.py
@@ -7,11 +7,12 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import urllib2
+import time
 
 
 import re
 
-tjson = []
+
 
 def getPDFUrl(infoCode,rDate):
     from dateutil.parser import parse
@@ -24,39 +25,39 @@ def getPDFUrl(infoCode,rDate):
         file_url = soup.find_all(text='查看PDF原文')[0].parent.get('href')
         return file_url
     except:
-        print('get pdf url failed ' + tmp_url + '\n')
+        print('get-pdf-url-failed ' + tmp_url)
         pass
 
-# pdfurl = getPDFUrl(u'APPH5Ri4N5KVASearchReport',u'2016-09-23T15:54:40')
-#
-# print pdfurl
+def scanRange(start,end):
+    tjson = []
+    for i in range(start,end):
+        raw_request = requests.get('http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=GGSR&js=var%20KFhOckHz={%22data%22:[(x)],%22pages%22:%22(pc)%22,%22update%22:%22(ud)%22,%22count%22:%22(count)%22}&ps=50&p='+str(i)+'&mkt=0&stat=0&cmd=2&code=&rt=49135668')
+        p = re.compile('var KFhOckHz=(.*)')
+        m = p.match(raw_request.text)
+        jobj = json.loads(m.group(1),'utf-8')
+        raw_records = jobj[u'data']
 
-for i in range(1,911):
-    raw_request = requests.get('http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=GGSR&js=var%20KFhOckHz={%22data%22:[(x)],%22pages%22:%22(pc)%22,%22update%22:%22(ud)%22,%22count%22:%22(count)%22}&ps=50&p='+str(i)+'&mkt=0&stat=0&cmd=2&code=&rt=49135668')
-                               # "http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=GGSR&js=var%20sfLadxId={%22data%22:[(x)],%22pages%22:%22(pc)%22,%22update%22:%22(ud)%22,%22count%22:%22(count)%22}&ps=50&p=3&mkt=0&stat=0&cmd=4&code=&rt=49162654"
-    # print raw_request.text
-    #
-    p = re.compile('var KFhOckHz=(.*)')
+        for record in raw_records:
+            tjson.append({"author":record[u"author"],"infoCode":record[u"infoCode"],"datetime":record[u"datetime"],"insName":record[u"insName"],"secuName":record[u"secuName"],"secuFullCode":record[u"secuFullCode"],"title":record[u"title"],"pdfurl": getPDFUrl(record[u"infoCode"],record[u"datetime"])})
+            # print ({"author":record[u"author"],"infoCode":record[u"infoCode"],"datetime":record[u"datetime"],"insName":record[u"insName"],"secuName":record[u"secuName"],"secuFullCode":record[u"secuFullCode"],"title":record[u"title"],"pdfurl": getPDFUrl(record[u"infoCode"],record[u"datetime"])})
+    return tjson
 
-    m = p.match(raw_request.text)
+#all = []
 
-    # print m.group(1)
+for k in range(20,910,10):
+    print k
+    tmp = scanRange(k,k+10)
+    #all.extend(tmp)
+    #time.sleep(10*60)
 
-    jobj = json.loads(m.group(1),'utf-8')
+    with open('data.out', 'a') as outfile:
+        for item in tmp:
+            json.dump(item,outfile)
+            outfile.write(',\n')
+    #all = []
+    # json.dump(tjson, outfile)
+    time.sleep(10*60)
 
-    raw_records = jobj[u'data']
-
-
-
-    for record in raw_records:
-
-        tjson.append({'author':record[u'author'],'infoCode':record[u'infoCode'],'datetime':record[u'datetime'],'insName':record[u'insName'],'secuName':record[u'secuName'],'secuFullCode':record[u'secuFullCode'],'title':record[u'title'],'pdfurl': getPDFUrl(record[u'infoCode'],record[u'datetime'])})
-        print ({'author':record[u'author'],'infoCode':record[u'infoCode'],'datetime':record[u'datetime'],'insName':record[u'insName'],'secuName':record[u'secuName'],'secuFullCode':record[u'secuFullCode'],'title':record[u'title'],'pdfurl': getPDFUrl(record[u'infoCode'],record[u'datetime'])})
-
-# with open('data.json', 'w') as outfile:
-#     json.dump(tjson, outfile)
-
-# print json.dumps(tjson)
 
 
 
